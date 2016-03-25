19af7dfdb564226f667dc3523c596165492e9412
CelsiusServa <celsius.serva@gmail.com>
2016-03-25 19:29:27 -0300

    - improved UI

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -2,13 +2,19 @@
     "use strict";
 
     angular.module('finalFantasy.pages')
-        .controller('HomeController', ['$route', controller]);
+        .controller('HomeController', ['$route', '$timeout', controller]);
 
-    function controller($route) {
+    function controller($route, $timeout) {
         var vm = this;
+        $timeout(function() {
+            $('.parallax-window.banner').parallax({imageSrc: 'assets/images/fc_house.png', speed: '0', positionY: '-200px' });
+            $('#content1.parallax-window.content-block').parallax({imageSrc: 'assets/images/a5.png', speed: '0.2' });
+            $('#content2.parallax-window.content-block').parallax({imageSrc: 'assets/images/a6.png', speed: '0.2', position: 'top' });
+            $('#content3.parallax-window.content-block').parallax({imageSrc: 'assets/images/a7.png', speed: '0.2', position: 'top' });
+        }, 100);
+
 
         var routeParams = $route.current.params;
-
         vm.articleIndex = 'titanArticle';
     }
 }());
