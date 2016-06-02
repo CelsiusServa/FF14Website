8746e45979ab43f2488e026b1a5ba541ca1a2ef3
CelsiusServa <celsius.serva@gmail.com>
2016-06-01 21:48:37 -0300

    - added upcoming events article

--- app/utils/article/article.service.js
+++ app/utils/article/article.service.js
@@ -58,7 +58,35 @@
                         type: 'text',
                         text: '~Lilli Gant'
                     }]
-            }
+            },
+            upcomingEvents: {
+                title: 'Upcoming Events',
+                articleId: 'upcomingEvents',
+                author: 'Lilli Gant',
+                image: 'final-fantasy-xiv-upcoming-events.jpg',
+                date: Date.parse("6/1/2016 10:02 PM"),
+                blocks: [
+                    {
+                        type: 'text',
+                        text: 'Hello FC friends! o7 with patch 3.2 wrapping up we have a ton of FC events for all walks of life coming up for FC bonding and what not!'
+                    },
+                    {
+                        type: 'text',
+                        text: 'First of all welcome to all our new members this week, Megumi Megu, Etoile Mourante, Chin\'chin Bin, Xelest Yesia and Navi Blue! We’re super happy to have you join us and hope you enjoy your time here!'
+                    },
+                    {
+                        type: 'text',
+                        text: 'A big thank you to Riku Kurayami for buying  VIP membership with the Gilded Pony for the entire FC!  For those interested there is an event on the 11th of June that you’re more than welcome to attend. Message Riku Kurayami in game for more information.'
+                    },
+                    {
+                        type: 'text',
+                        text: 'Next is a personal project of mine, I’m looking to see if we can get some in house 4v4 pvp matches going, if we can get enough interest I’ll put an offical date for when we can beat the shit out of each other! Just message me in game (Lilli Gant) with what role you would want to play and hopefully we can get enough people!  If you’re new to pvp or a vet it doesn’t matter this is entirely an event for fun!'
+                    },
+                    {
+                        type: 'text',
+                        text: 'Last but def not least, on the 18th/19th of June we have a Nidhogg EX learning/clear event!  Those of us in FC statics are taking time out of our busy *cough* raiding lives to help other fc members learn/clear Nidhogg EX so we don’t have to all suffer in Party Finder \o/ Message Ka Lin in game for more information and to reserve a spot!'
+                    }]
+            },
         };
 
         return {
